fb296fc96cd6bb963a747cbfb8732209e5b2598e
JT Smith <nexxai@gmail.com>
2023-10-18 12:18:58 -0600

    Missed the admin dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,6 +1,6 @@
 class Admin::DashboardController < ApplicationController
-  http_basic_authenticate_with name: "Jungle", password: "book"
-  
+  http_basic_authenticate_with name: ENV['BASIC_AUTH_USERNAME'], password: ENV['BASIC_AUTH_PASSWORD']
+
   def show
   end
 end
