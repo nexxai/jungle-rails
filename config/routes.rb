27745136a163418e78008c63b48bba437edfe4ea
JT Smith <nexxai@gmail.com>
2023-10-19 11:27:25 -0600

    Adding User model and related items

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,9 @@ Rails.application.routes.draw do
     resources :products, except: [:edit, :update, :show]
     resources :categories, except: [:destroy, :edit, :update, :show]
   end
+  
+  get '/signup' => 'users#new'  
+  post '/users' => 'users#create'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
