4bf8dac86c498a09c5858e58fb87aab66bec3bec
JT Smith <nexxai@gmail.com>
2023-10-23 15:38:28 -0600

    Adding tests to User model

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,14 @@ class SessionsController < ApplicationController
   def new
   end
 
-    def create
-    user = User.find_by_email(params[:email])
-    if user && user.authenticate(params[:password])
+  def create
+        # user = User.find_by_email(params[:email])
+    # if user && user.authenticate(params[:password])
+
+    # else
+    #   redirect_to '/login'
+    # end
+    if user = User.authenticate_with_credentials(params[:email], params[:password])
       session[:user_id] = user.id
       redirect_to '/'
     else
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,15 @@
 class User < ApplicationRecord
   has_secure_password
+
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :password, presence: true, confirmation: true, length: { minimum: 9 }
+  validates :password_confirmation, presence: true
+
+  def self.authenticate_with_credentials(email, password)
+    @user = User.where('lower(email) = lower(?)', email.strip).first
+    if @user && @user.authenticate(password)
+      @user
+    end
+  end
 end
