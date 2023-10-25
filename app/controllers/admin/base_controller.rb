e3fb2d5a5c40ac6b387f50f1bab8d394d2c65555
JT Smith <nexxai@gmail.com>
2023-10-25 15:08:26 -0600

    Extending all admin controllers from Admin::BaseController

---
+++ app/controllers/admin/base_controller.rb
@@ -0,0 +1,3 @@
+class Admin::BaseController < ApplicationController
+  http_basic_authenticate_with name: ENV['BASIC_AUTH_USERNAME'], password: ENV['BASIC_AUTH_PASSWORD']
+end
--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,6 +1,4 @@
-class Admin::CategoriesController < ApplicationController
-  http_basic_authenticate_with name: ENV['BASIC_AUTH_USERNAME'], password: ENV['BASIC_AUTH_PASSWORD']
-
+class Admin::CategoriesController < Admin::BaseController
   def index
     @categories = Category.order(id: :desc).all
   end
